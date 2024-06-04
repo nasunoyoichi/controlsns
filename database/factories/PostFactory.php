d57b1ae593fec53185b07f98251bde0f802931ea
nasunoyoichi <nasunoyoichi.1030@gmail.com>
2024-06-04 11:52:17 +0900

    postのダミーデータとpostを取得するapiを作成しました。

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'post_id' => $this->faker->randomNumber(),
+            'user_id' => $this->faker->randomNumber(),
+            'content' => $this->faker->text(maxNbChars: 255),
+        ];
+    }
+}
