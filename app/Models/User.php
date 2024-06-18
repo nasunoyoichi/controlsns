e4b09c6ed7a1deed3d87feedb6a53a9e671cf126
nasunoyoichi <nasunoyoichi.1030@gmail.com>
2024-06-18 12:23:52 +0900

    postがソフトデリートできるようにしました。fillableのカラムの追加をしました。

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'screen_name',
+        'profile_image',
         'email',
         'password',
     ];
