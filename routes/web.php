d6f4ff4fc636201e96fbc287a83ce7678af71ad8
nasunoyoichi <nasunoyoichi.1030@gmail.com>
2024-06-19 12:36:00 +0900

    認証機能を追加しました

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-use Illuminate\Http\Request;
-use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +17,15 @@ use App\Models\Post;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/discover', function () {
-    return view('Discover');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/discover-data', function () {
-    $posts = Post::all(); 
-    return response()->json($posts);
-});
-
+require __DIR__.'/auth.php';
